b9e2590b02d501ce92908292216210fb61ebd9cf
Anastasiia Kharytonova <114058152+anasteisha3j@users.noreply.github.com>
2026-05-09 22:55:33 +0300

    Delete project directory

--- project/core/__init__.py
+++
@@ -1,7 +0,0 @@
-# Ядро системи CanaryTrap
-from .token_generator import TokenGenerator
-from .deploy import DeployEngine
-from .monitor import Monitor
-from .alert_system import AlertSystem
-
-__all__ = ['TokenGenerator', 'DeployEngine', 'Monitor', 'AlertSystem']
